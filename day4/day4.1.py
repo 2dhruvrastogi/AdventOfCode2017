3c44844bbe5907b34360ce347edcd1c8849fb59e
Dhruv Rastogi <dhruv.rastogi@capitalone.com>
2017-12-12 10:28:22 -0600

    simpler way day4.1

--- day4/day4.1.py
+++ day4/day4.1.py
@@ -1,13 +1,22 @@
-f = open("input4.py", "r")
+# f = open("input4.txt", "r")
+# res = 0
+# for line in f:
+#     s = dict()
+#     curr_line = line.split()
+#     for i in curr_line:
+#         if i in s:
+#             s[i] += 1
+#         else:
+#             s[i] = 1
+#     if all(value == 1 for value in s.values()):
+#         res += 1
+# print(res)
+
+
+f = open("input4.txt", "r")
 res = 0
 for line in f:
-    s = dict()
     curr_line = line.split()
-    for i in curr_line:
-        if i in s:
-            s[i] += 1
-        else:
-            s[i] = 1
-    if all(value == 1 for value in s.values()):
+    if len(set(curr_line)) == len(curr_line):
         res += 1
 print(res)
